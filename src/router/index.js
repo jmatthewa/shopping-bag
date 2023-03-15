26674d378cf7f276aa53c6007972f1c309d8f20d
jmatthewa <jamesm.atanacio@gmail.com>
2023-03-15 03:10:12 -0700

    Initial commit with setup files

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import HomeView from '../views/HomeView.vue'
+import BasketView from '../views/BasketView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'HomeView',
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/basket',
+    name: 'BasketView',
+    component: BasketView
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 

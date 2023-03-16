41617bf8a3087c6ef0072a666a90b5131b68d89c
jmatthewa <jamesm.atanacio@gmail.com>
2023-03-16 04:46:25 -0700

    Implement 'Remove from Bag' feature

--- src/store/index.js
+++ src/store/index.js
@@ -15,6 +15,10 @@ export default createStore({
     },
     addToBag(state, product){
       state.productsInBag.push(product);
+    },
+    removeFromBag(state, productId) {
+      var updatedBag = state.productsInBag.filter(item => productId != item.id);
+      state.productsInBag = updatedBag;
     }
   },
   actions: {
@@ -29,6 +33,10 @@ export default createStore({
 
     addToBag({ commit }, product) {
       commit('addToBag', product);
+    },
+
+    removeFromBag({ commit }, productId) {
+      commit('removeFromBag', productId);
     }
 
   },
